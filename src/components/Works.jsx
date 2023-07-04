e138ef7f0a173377a6e40dc86e6c7cf7867c78c8
Edward Marzal <edsanchezmarzal@gmail.com>
2023-07-04 00:53:47 -0500

    coming soon projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -88,11 +88,25 @@ const Works = () => {
         </motion.p>
       </div>
 
+      {/* 
+
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
+
+       */}
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px] flex justify-center'
+        >
+          COMING SOON.
+
+        </motion.p>
+      </div>
+
     </>
   );
 };
